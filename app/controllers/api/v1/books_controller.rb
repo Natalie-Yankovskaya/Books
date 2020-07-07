7aab3ee05ad643eb6b77873c29cefbd41038ce6c
Natalie-Yankovskaya <natalie.yankovskaya0209@gmail.com>
2020-07-07 14:08:43 +0300

    Updated books_controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,76 +1,86 @@
 class Api::V1::BooksController < ApplicationController
 
-
 	#show books (all, true or false)
 	def index
 
-		book = Book.new(book_params)
-		book1 = book_params[:status]
-
-		if(book1 == true)
-
-		@books_true = Book.select(Arel.star).where(Book.arel_table[:status].eq(true))
-		render json: {status: 'SUCCESS', message: 'The books that are read', data: @books_true}
-
-	else
-		if(book1 == false)
-
-		@books_false = Book.select(Arel.star).where(Book.arel_table[:status].eq(false))
-				render json: {status: 'SUCCESS', message: 'The books that are unread', data: @books_false}
-
-	else
-
-		@books_all = Book.all
-		render json: {status: 'SUCCESS', message: 'All books', data: @books_all }
-
-	end		
-	end
-
+		if book_params[:status].nil?
+			books_all = Book.all
+			render json: {status: STATUS['success'], message: 'All books', data: books_all}
+		elsif(book_params[:status])
+			books_true = Book.where(status: true)
+			render json: {status: STATUS['success'], message: 'The books that are read', data: books_true}
+		else
+			books_false = Book.where(status: false)
+			render json: {status: STATUS['success'], message: 'The books that are unread', data: books_false}
+		end	
 
 	end
 
 
-#show book by index (пишем в строке: например .../12)
-def show
-	book = Book.find(params[:id])
-	render json: {status: 'SUCCESS', message: "Loaded article", data:book}
-end
+	#show book by index (пишем в строке: например .../12)
+	def show
+		book = Book.find(params[:id]) 
+
+		if book.nil?
+			render json: {status: STATUS['not_found'], message: "The book is not found", data: book.errors}
+		else
+			if params[:idx_add]
+				author = Author.find(params[:idx_add])
+				book.authors << author
+			end
+
+			if params[:idx_delete]
+				author = Author.find(params[:idx_delete])
+				book.authors.destroy(author)
+			end
+
+			render json: {status: STATUS['success'], message: "Loaded book", data: book, number_of_authors: book.authors.count }
+		end
+
+	end
 
 
 
-#Add book
+	#Add book
 	def create
-	book = Book.new(book_params)
-	if book.save
-		render json: {status: 'SUCCESS', message: "The book is created", data:book}
-	else
-		render json: {status: 'ERROR', message:'The book is not created', data:book.errors}
+		book = Book.new(book_params)
+		if book.save
+			render json: {status: STATUS['created'], message: "The book is created", data: book}
+		else
+			render json: {status: STATUS['bad_request'], message: "The book is not created", data: book.errors}
+		end
+
 	end
-end
 
 
-#delete book (пишем в строке: например .../12)
-def destroy
-	book = Book.find(params[:id])
-	book.destroy
-	render json: {status: 'SUCCESS', message: "The book is deleted", data:book}
-end
-
-
-#Mark as read
-def update
-	book = Book.find(params[:id])
-	if book
-		book.update(:status => 'true')
-	render json: {status: 'SUCCESS', message: "The book is marked as read", data:book}
-	else
-		render json: {status: 'ERROR', message:'The book is not updated', data:book.errors}
+	#delete book (пишем в строке: например .../12)
+	def destroy
+		book = Book.find(params[:id])
+		if book.nil? 
+			render json: {status: STATUS['not_found'], message: "The book is not found", data: book.errors}
+		else
+			book.destroy
+			render json: {status: STATUS['success'], message: "This book is deleted", data: book}
+		end
 	end
-end
 
 
-private
-def book_params
-	params.permit(:id, :title, :author, :status)
-end
+	#Mark as read
+	def update
+		book = Book.find(params[:id])
+		if book
+			book.update(:status => 'true')
+			render json: {status: STATUS['success'], message: "The book is marked as read", data: book}
+		else
+			render json: {status: STATUS['not_found'], message:"The book is not found", data: book.errors}
+		end
+	end
+
+
+	private
+	def book_params
+		params.permit(:id, :title, :status)
+	end
+
+
 end
